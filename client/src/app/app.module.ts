5f3675edf859de8ba0a5f7efe473f3b9c0471496
Muhammad Azizol Aminuddin <Bjoe88@gmail.com>
2019-10-02 04:18:57 +0800

    add angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,21 @@ import { AppComponent } from './app.component';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { FormsModule } from '@angular/forms';
+
+
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatGridListModule
+} from "@angular/material";
+
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
@@ -19,6 +34,16 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatGridListModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],
